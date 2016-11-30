41c2127fb6f269f29325dea794bfc333e073ace9
selmanbey <selmannur@yahoo.com>
2016-11-30 14:26:55 +0000

    1st Commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+/*cv*/
+$(".cv").hover(
+       function () {$("#head1").css("visibility", "visible")},
+         function () {$("#head1").css("visibility", "hidden") 
+                     });
+/*
+$(".cv").hover(
+       function () {$(".header, .footer").css("background-color", "#7e4e31")},
+         function () {$(".header, .footer").css("background-color", "#111")
+                     }); */
+
+/*philosophy-stuff*/
+$(".philosophy-stuff").hover(
+       function () {$("#head2").css("visibility", "visible")},
+         function () {$("#head2").css("visibility", "hidden") 
+                     });
+/*
+$(".philosophy-stuff").hover(
+       function () {$(".header, .footer").css("background-color", "#812933")},
+         function () {$(".header, .footer").css("background-color", "#111") 
+                     }); */
+
+/*photography-stuff*/
+
+$(".photography-stuff").hover(
+       function () {$("#head3").css("visibility", "visible")},
+         function () {$("#head3").css("visibility", "hidden") 
+                     });
+
+/*
+$(".photography-stuff").hover(
+       function () {$(".header, .footer").css("background-color", "#363d55")},
+         function () {$(".header, .footer").css("background-color", "#111") 
+                     }); */
+
+/*other*/
+$(".other").hover(
+       function () {$("#head4").css("visibility", "visible")},
+         function () {$("#head4").css("visibility", "hidden") 
+                     });
+/*
+$(".other").hover(
+       function () {$(".header, .footer").css("background-color", "#1b684c")},
+         function () {$(".header, .footer").css("background-color", "#111") 
+                     }); */
