109c250fc59de31cdb4ccf05adb1a5c7ed3eae99
selmanbey <selmannur@yahoo.com>
2018-02-28 05:16:29 +0000

    entire layout change. media queries are missing

--- js/index.js
+++ js/index.js
@@ -1,40 +1,53 @@
-/*cv-button*/
-$(".cv").hover(
-       function () {$("#head1").css("visibility", "visible")},
-         function () {$("#head1").css("visibility", "hidden")
-                     });
+document.addEventListener("DOMContentLoaded", function(){
 
-/*philosophy-stuff-button*/
-$(".philosophy-stuff").hover(
-       function () {$("#head2").css("visibility", "visible")},
-         function () {$("#head2").css("visibility", "hidden") 
-                     });
+  const japanCircle = document.querySelector(".japan-circle");
+  const whoAmI = document.querySelector(".who-am-i");
+  const navContainer = document.querySelector(".nav-container");
+  const allIcons = document.querySelectorAll(".icon");
+  const allIconLinks = document.querySelectorAll(".icon-link");
+  const iconExplanations = document.querySelectorAll(".icon-explanation");
 
-/*photography-stuff-button*/
+  whoAmI.addEventListener("mouseover", function() {japanCircle.classList.toggle("animate")})
+  whoAmI.addEventListener("mouseout", function() {japanCircle.classList.toggle("animate")})
+  whoAmI.addEventListener("click", function() {navContainer.classList.toggle("animate")})
+  
+  for(let i = 0; i < allIconLinks.length; i++){
+    allIconLinks[i].addEventListener("mouseover", function() {
+      iconExplanations[i].style.display = "inline-block";
+    });
+    allIconLinks[i].addEventListener("mouseout", function() {
+      iconExplanations[i].style.display = "none";
+    })
 
-$(".photography-stuff").hover(
-       function () {$("#head3").css("visibility", "visible")},
-         function () {$("#head3").css("visibility", "hidden") 
-                     });
+    // THIS PART DOES NOT WORK! COULDN'T FIGURE OUT WHY.
+    allIconLinks[i].addEventListener("click", function(e) {
+      e.preventDefault();
+      allIcons[i].style.animation = "icon-clicked 100ms linear infinite";
+    })
+  }
 
+  /// POSSIBLE IMPLEMENTATION
 
-/*other-button*/
-$(".other").hover(
-       function () {$("#head4").css("visibility", "visible")},
-         function () {$("#head4").css("visibility", "hidden") 
-                     });
+  // const backgroundImages = ["pics/kuzey_daglari.jpg", "pics/nazifin_istanbulu.jpg", "pics/bogazicinden.jpg", "pics/marmarada_bir_kose.jpg"]
 
-/*SMOOTH SCROLLING --admittedly, I've just copy-pasted this from another website: I really don't understand how and why the code works here. But hey! It works :) */
-$(function() { $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+  // const lastScreen = document.querySelector(".last-screen")
+  // const nextScreen = document.querySelector(".next-screen")
+  // const background = document.querySelector(".background")
+
+  // let backgroundImageIndex = 0;
+  // nextScreen.addEventListener("click", function(){
+  //   if(backgroundImageIndex === backgroundImages.length - 1) {
+  //   } else {
+  //     backgroundImageIndex += 1;
+  //     background.style.backgroundImage = `url(${backgroundImages[backgroundImageIndex]})`;
+  //   }
+  // })
+  // lastScreen.addEventListener("click", function(){
+  //   if(backgroundImageIndex === 0) {
+  //   } else {
+  //     backgroundImageIndex -= 1;
+  //     background.style.backgroundImage = `url(${backgroundImages[backgroundImageIndex]})`;
+  //   }
+  // })
+  
+})
