d1b35176074b98662cfee5aa216ec4f89245a791
selmanbey <selmannur@yahoo.com>
2018-04-21 13:30:07 +0100

    refactored

--- js/index.js
+++ js/index.js
@@ -1,29 +1,57 @@
+const view = {
+  japanCircle: document.querySelector(".japan-circle"),
+  whoAmI: document.querySelector(".who-am-i"),
+  navContainer: document.querySelector(".nav-container"),
+  allIcons: document.querySelectorAll(".icon"),
+  allIconLinks: document.querySelectorAll(".icon-link"),
+  iconExplanations: document.querySelectorAll(".icon-explanation"),
+};
+
+const controller = {
+  init: function() {
+    this.addEventListeners();
+  },
+
+  addEventListeners: function() {
+    this.events.japanCircleToggle();
+    this.events.navContainerToggle();
+    this.events.activateIcons();
+  },
+
+  events: {
+    japanCircleToggle: function() {
+      view.whoAmI.addEventListener("mouseover", function() {view.japanCircle.classList.toggle("animate")});
+      view.whoAmI.addEventListener("mouseout", function() {view.japanCircle.classList.toggle("animate")});
+    },
+
+    navContainerToggle: function() {
+      view.whoAmI.addEventListener("click", function() {view.navContainer.classList.toggle("animate")});
+    },
+
+    activateIcons: function() {
+      for(let i = 0; i < view.allIconLinks.length; i++){
+        view.allIconLinks[i].addEventListener("mouseover", function() {
+          view.iconExplanations[i].style.display = "inline-block";
+        });
+        view.allIconLinks[i].addEventListener("mouseout", function() {
+          view.iconExplanations[i].style.display = "none";
+        });
+        // THIS PART DOES NOT WORK! COULDN'T FIGURE OUT WHY.
+        view.allIconLinks[i].addEventListener("click", function(e) {
+          view.allIcons[i].style.animation = "icon-clicked 100ms linear infinite";
+        });
+      };
+    },
+  },
+};
+
 document.addEventListener("DOMContentLoaded", function(){
+  controller.init();
+});
 
-  const japanCircle = document.querySelector(".japan-circle");
-  const whoAmI = document.querySelector(".who-am-i");
-  const navContainer = document.querySelector(".nav-container");
-  const allIcons = document.querySelectorAll(".icon");
-  const allIconLinks = document.querySelectorAll(".icon-link");
-  const iconExplanations = document.querySelectorAll(".icon-explanation");
 
-  whoAmI.addEventListener("mouseover", function() {japanCircle.classList.toggle("animate")})
-  whoAmI.addEventListener("mouseout", function() {japanCircle.classList.toggle("animate")})
-  whoAmI.addEventListener("click", function() {navContainer.classList.toggle("animate")})
-  
-  for(let i = 0; i < allIconLinks.length; i++){
-    allIconLinks[i].addEventListener("mouseover", function() {
-      iconExplanations[i].style.display = "inline-block";
-    });
-    allIconLinks[i].addEventListener("mouseout", function() {
-      iconExplanations[i].style.display = "none";
-    })
 
-    // THIS PART DOES NOT WORK! COULDN'T FIGURE OUT WHY.
-    allIconLinks[i].addEventListener("click", function(e) {
-      allIcons[i].style.animation = "icon-clicked 100ms linear infinite";
-    })
-  }
+
 
   /// POSSIBLE IMPLEMENTATION
 
@@ -48,5 +76,3 @@ document.addEventListener("DOMContentLoaded", function(){
   //     background.style.backgroundImage = `url(${backgroundImages[backgroundImageIndex]})`;
   //   }
   // })
-
-})
